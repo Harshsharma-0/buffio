90631a433a5f14bd233fae2623e795e8ae660b09
HarshSharma-0 <aparna6008in@gmail.com>
2025-10-17 12:26:23 +0530

    buffio project

---
+++ Makefile
@@ -0,0 +1,2 @@
+main:main.cpp buffio.hpp
+	 g++ -std=c++20 main.cpp -o main && ./main
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define BUFFIO_IMPLEMENTATION
+#define BUFFIO_DEBUG
+#include "./buffio.hpp"
+
+
+int main(){
+  buffioinfo serverinfo = {
+   .address = "127.0.0.1",
+   .portnumber = 8080,
+   .maxclient = 500,
+   .socktype = BUFFIO_SOCK_TCP,
+   .sockfamily = BUFFIO_FAMILY_IPV4
+  };
+
+  buffio::buffsocket server(serverinfo);
+  system("sleep 30s");
+ };
