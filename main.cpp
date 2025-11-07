f0b5150f3e9a0ca84ea141d8b9bb901b702eac34
HarshSharma-0 <aparna6008in@gmail.com>
2025-11-08 00:27:13 +0530

    completed buffio

--- main.cpp
+++ main.cpp
@@ -4,20 +4,38 @@
 #include "./buffio.hpp"
 
 
+buffioroutine clientcall(){
+ std::cout<<"hello client2"<<std::endl;
+ co_return 0;
+};
+
+buffioroutine clienthandler(clientinfo info){
+
+  std::cout<<"hello client : "<<info.address<<":"<<info.portnumber<<std::endl;
+  iowait clientcall();
+  std::cout<<"hello client exit"<<std::endl;
+  ioreturn 0;
+};
+
 int main(){
-  /*
+  
   buffioinfo serverinfo = {
    .address = "127.0.0.1",
-   .portnumber = 8080,
-   .maxclient = 500,
+   .portnumber = 8081,
+   .listenbacklog = 10,
+   .capacity = 10,
+   .reserve = 10,
    .socktype = BUFFIO_SOCK_TCP,
    .sockfamily = BUFFIO_FAMILY_IPV4
   };
 
   buffio::buffsocket server(serverinfo);
-  */
-
+  server.clienthandler = clienthandler;
+  
+  
   buffio::instance runner;
-  runner.fireeventloop(); 
+  runner.fireeventloop(server); 
+  
+
   return 0;
  };
