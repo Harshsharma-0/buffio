65213cfa458d01d123edcef9578ff457fd301c51
HarshSharma-0 <aparna6008in@gmail.com>
2025-11-20 20:30:48 +0530

    adding support for lockfree queue for epoll

--- buffio.hpp
+++ buffio.hpp
@@ -54,7 +54,7 @@ enum BUFFIO_QUEUE_STATUS {
 enum BUFFIO_EVENTLOOP_TYPE {
   BUFFIO_EVENTLOOP_SYNC = 50, // use this to block main thread
   BUFFIO_EVENTLOOP_ASYNC,     // use this to launch a thread;
-  BUFFIO_EVENTLOOP_SEPERATE,  // use this to create a seperate process from main
+ // BUFFIO_EVENTLOOP_SEPERATE,  // use this to create a seperate process from main
   BUFFIO_EVENTLOOP_DOWN,      // indicates eventloop is not running
 
 };
@@ -550,7 +550,7 @@ private:
       threadstatus = BUFFIO_THREAD_ERROR;
       BUFFIO_ERROR("Failed to create thread , reason -> "
                   ,strerror(errno),
-                  threadname == nullptr ? "error","thread name ->",threadname);
+                  threadname == nullptr ? "error":"thread name ->",threadname);
       
       reset();
       return;
@@ -569,14 +569,21 @@ private:
   int (*callfunc)(void *);
 };
 
+
+class buffiolfqueue{
+
+public:
+private:
+
+};
+
 #include <sys/epoll.h>
 // socket broker used to listen for events in socket;
 // internally uses epoll for all work;
 constexpr int BUFFIO_POLL_READ = EPOLLIN;
 constexpr int BUFFIO_POLL_WRITE = EPOLLOUT;
 constexpr int BUFFIO_POLL_ETRIG = EPOLLET;
-
-#define BUFFIO_EPOLL_MAX_THRESHOLD 100
+constexpr size_t BUFFIO_EPOLL_MAX_THRESHOLD = 100;
 
 enum BUFFIO_SOCKBROKER_STATE {
   BUFFIO_SOCKBROKER_ACTIVE = 71,
@@ -597,13 +604,15 @@ public:
   
   int start() {
     switch (sbrokerstate) {
+      
     case BUFFIO_SOCKBROKER_INACTIVE:
-      epollfd = epoll_create(0);
-      if (epollfd < 0) {
-        BUFFIO_ERROR("Failed to create a epoll instance of socker : reason -> ",
-                     strerrorno(errno))
+      epollstate.epollfd = epoll_create(1);
+      if (epollstate.epollfd < 0) {
+        BUFFIO_ERROR(" Failed to create a epoll instance of socker : reason -> ",
+                     strerror(errno));
         return BUFFIO_SOCKBROKER_ERROR;
       };
+         thread.run();
       break;
     case BUFFIO_SOCKBROKER_ACTIVE:
     case BUFFIO_SOCKBROKER_BUSY:
@@ -617,13 +626,13 @@ public:
     event.events = broker->event;
     event.data.fd = broker->fd;
     event.data.ptr = broker->task;
-
+  
     switch (sbrokerstate) {
     case BUFFIO_SOCKBROKER_ACTIVE:
-      int ret = epoll_ctl(epollfd, EPOLL_CTL_ADD, broker->fd, &event);
-      if (ret < 0) {
-        BUFFIO_ERROR("Failed to add file descriptor in epoll, reason : ",
-                     strerrno(errno));
+      int ret = epoll_ctl(epollstate.epollfd, EPOLL_CTL_ADD, broker->fd, &event);
+      if (ret < 0){
+        BUFFIO_ERROR(" Failed to add file descriptor in epoll, reason : ",
+                     strerror(errno));
         break;
       }
       return BUFFIO_SOCKBROKER_SUCCESS;
@@ -631,38 +640,68 @@ public:
     }
     return BUFFIO_SOCKBROKER_ERROR;
   };
+ // consumed = -1, indicate empty;
+ // consumed = 0, all ok;
+ // consumed = 1, epoll error;
+ // consumed = 2, epoll continue;
+ // consumed = 3 epoll event available;
+ // consumed = 4 parent consuming epoll;
+ 
+  struct epollcaller{
+   std::atomic<int> consumed;
+   int epollfd;
+   size_t fdcount;
+   size_t eventcount;
+   struct epoll_event *events[2];
+   struct epoll_event *available;
+   struct epoll_event *free;
+  };
 
   static int epolllistener(void *data) { 
-    buffiosockbroker *instance = (buffiosockbroker*)data;
-    
-    std::cout<<"hello epoll"<<std::endl;
+    struct epollcaller *estate = (struct epollcaller*)data;
+    int numfds = epoll_wait(estate->epollfd,estate->events[0],estate->eventcount,-1);  
+    if(numfds < 0){
+      estate->consumed.store(1, std::memory_order_release);
+      BUFFIO_ERROR(" epoll wait error, reason : ", strerror(errno));
+    };
+
+     estate->consumed.store(1, std::memory_order_release);
+
     return 0; 
   };
 
   ~buffiosockbroker() {
+
     switch (sbrokerstate) {
     case BUFFIO_SOCKBROKER_INACTIVE: return;
-    case BUFFIO_SOCKBROKER_ACTIVE:
+    case BUFFIO_SOCKBROKER_ACTIVE:  
+         thread.killthread();
+         close(epollstate.epollfd);
+      break;
     case BUFFIO_SOCKBROKER_BUSY:
       break;
     }
   };
 
-  buffiosockbroker()
-      : sbrokerstate(BUFFIO_SOCKBROKER_INACTIVE), fdcount(0), eventcount(0) {
-    memset(&events, '\0', sizeof(epoll_event) * BUFFIO_EPOLL_MAX_THRESHOLD);
-    consumed = -1; 
+  buffiosockbroker(size_t maxevents)
+      : sbrokerstate(BUFFIO_SOCKBROKER_INACTIVE){
+
+     epollstate.events[0] = new struct epoll_event[maxevents];
+     epollstate.events[1] = new struct epoll_event[maxevents];
+     epollstate.available = nullptr;
+     epollstate.free = nullptr;
+     epollstate.eventcount = maxevents;
+     epollstate.fdcount = 0;
+     epollstate.epollfd = -1;
     thread[buffiothread::SD]["buffiosocketbroker"](this) = epolllistener;
+
   };
 
 private:
   buffiothread thread;
+  struct epollcaller epollstate;
+  size_t epollmaxevent;
   int sbrokerstate;
-  int epollfd;
-  size_t fdcount;
-  size_t eventcount;
-  std::atomic<int> consumed;
-  struct epoll_event events[BUFFIO_EPOLL_MAX_THRESHOLD];
 };
 
 
@@ -672,6 +711,9 @@ public:
   static int eventloop(void *data) {
     buffioinstance *instance = (buffioinstance *)data;
     buffioqueue<buffiotaskinfo, buffioroutine> *queue = &instance->iqueue;
+    
+    buffiosockbroker iobroker(BUFFIO_EPOLL_MAX_THRESHOLD); 
+
     while (queue->empty() == false) {
       buffiotaskinfo *taskinfo = queue->getnextwork();
       if (taskinfo == nullptr)
@@ -786,7 +828,6 @@ private:
      to->handle.promise().childstatus = from->handle.promise().selfstatus;
   };
   buffioqueue<buffiotaskinfo, buffioroutine> iqueue;
-  buffiosockbroker iobroker; 
   enum BUFFIO_EVENTLOOP_TYPE ieventlooptype;
 };
 
