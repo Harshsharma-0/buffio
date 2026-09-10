be9d52891b2e8d094bc3524273a54b902726c05e
Harsh Sharma <aparna6008in@gmail.com>
2026-09-10 17:04:04 +0100

    adds todos

--- src/backend/fs/windows.cpp
+++ src/backend/fs/windows.cpp
@@ -3,25 +3,63 @@
 #include "buffio/worker.hpp"
 
 bool buffio::File::OpenStdIn(){
-  assert(fd <= BUFFIO_FD_INVALID);
-  fd = STDIN_FILENO;
+  assert(fd == BUFFIO_FD_INVALID);
+  buffio_fd tmp_fd = GetStdHandle(STD_INPUT_HANDLE);
+  if(tmp_fd == INVALID_HANDLE_VALUE)
+      return false;
+  fd = tmp_fd;
   return true; 
 };
 
 bool buffio::File::OpenStdOut(){ 
-  assert(fd <= BUFFIO_FD_INVALID);
-  fd = STDOUT_FILENO;
+  assert(fd == BUFFIO_FD_INVALID);
+  buffio_fd tmp_fd = GetStdHandle(STD_OUTPUT_HANDLE);
+  if(tmp_fd == INVALID_HANDLE_VALUE)
+    return false;
+
+  fd = tmp_fd;
   return true;
 };
 
-int buffio::OpenFileAwaiter::await_resume(){ 
-  if(op_state.fd == BUFFIO_FD_INVALID) return -1;
 
-  buffio::File *file = 
-       static_cast<buffio::File*>(rval); 
+bool buffio::OpenFileAwaiter::action(std::pair<void*,void*> info){
+  auto[p_sqe,p_self] = info;
+  buffio::OpenFileAwaiter *obj =
+           static_cast<buffio::OpenFileAwaiter *>(p_self);
+  
+           /*
+  int fd = open(obj->path,obj->flags,(mode_t)obj->mode); 
+  if(fd < 0){
+    obj->op_state.fd = BUFFIO_FD_INVALID;
+    return true;
+  };
 
-  file->fd = op_state.fd;
-  file->loffset = 0;
 
-  return 0;
+  obj->op_state.fd = fd;
+  */
+  
+  return true;
 };
+
+bool buffio::AwaitableFileBase::action(std::pair<void*,void*> info){
+
+ auto[p_sqe,p_self] = info;
+ buffio::AwaitableFileBase *obj =
+               static_cast<buffio::AwaitableFileBase *>(p_self);
+
+ auto[fd,buffer,size,offset] = obj->state;
+ ssize_t rval = 0;
+
+ /*
+ switch(obj->op_state.op_code){
+  case OpCode::Read:  rval = pread(fd,buffer,size,*offset); break;
+  case OpCode::Write: rval = pwrite(fd,buffer,size,*offset); break;
+  case OpCode::Readv: rval = preadv(fd,(struct iovec*)buffer,size,*offset); break;
+  case OpCode::Writev: rval = pwritev(fd,(struct iovec*)buffer,size,*offset); break;
+  default: rval = -1; break;
+ };
+ */
+  obj->op_state.op_done = rval;
+  return true;
+};
+
--- src/backend/worker/epoll.cpp
+++ src/backend/worker/epoll.cpp
@@ -12,31 +12,6 @@ void buffio::Worker::signalLoop(buffio_fd fd,LoopStatusCode code){
       (void)ret;
 };
 
-int buffio::Worker::init(int numWorker, unsigned int queueSize) {
-
-  /* evaluating the maximum worker thread that can concurrently access the
-   * queue*/
-  auto [maxWorker, order] = buffio::utility::get_pow2(queueSize);
-
-  /* checking it the maxWorker exceeds the maximun supported worker */
-  maxWorker = maxWorker < BUFFIO_MAX_WORKER ? maxWorker : BUFFIO_MAX_WORKER;
-
-  /* checking numWorker for negative value */
-  numWorker = numWorker <= 0 ? 4 : numWorker;
-
-  /* if numWorker exceed maxWorker set it to max worker */
-  numWorker = numWorker > maxWorker ? maxWorker : numWorker;
-  state.completion_lock.post(maxWorker);
-  
-  if (init_task_queues(order) != 0)
-    return -1;
-  if (init_poller(order) != 0)
-    return -2;
-  if (init_worker_threads(numWorker) != 0)
-    return -3;
-
-  return 0;
-};
 
 int buffio::Worker::init_poller(unsigned int order) {
 
